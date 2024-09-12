df21cabe80a383bd21e5624f537b6004b67d71cc
AnaBizo <aninhadealmeida0502@gmail.com>
2024-09-11 21:52:32 -0300

    iniciando projeto e adicionando inquire

--- index.js
+++ index.js
@@ -1,48 +1,21 @@
-// olá, mundo!
-console.log("olá, mundo!")
+const start = () => {
 
+    while(true) {
 
-// variáveis - let
-let mensagens = "olá, mundo!"
-
-
-// variáveis - const
-const mensagem = "olá, eu"
-{
-    const mensagem = "olá, Mayk!"
-    console.log(mensagem)
-}
-console.log(mensagem);
-
-
-// arrays
-let metas = ['mayk', 'alô']
-let metas01 = [2, 'mayk']
-// concatenando valores
-console.log(metas[1] + ", " + metas[0]) 
-
-let metas02 = [
-    metas,
-    {
-        value: 'caminhar 20 minutos todos os dias',
-        checked: false
+        let opcao = "cadastrar"
+        switch (opcao) {
+            case "cadastrar":
+                console.log("vamos cadastrar")
+                break
+            case "listar":
+                console.log("vamos listar")
+                break
+            case "sair":
+                return
+            
+        }
     }
-]
-console.log(metas02[1].value);
 
-// objetos
-let meta = {
-    value: 'ler um livro por mês',
-    address: 2,
-    checked: true,
-    isChecked: () => {
-        console.log(info)
-    }
 }
-console.log(meta.value);
 
-// function // arrow function
-const criarMeta = () => {}
-
-// named function
-function criarMetas() {}
+start()
